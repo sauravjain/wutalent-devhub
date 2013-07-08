5a2bed20670e67ff144a4af8d29e8f601f87e237
sauravjain <saurav@anylinuxwork.com>
2013-07-08 16:36:46 +0530

    After post the mailchimp key form, handle the key through mailchimp api's files

---
+++ plugin_code/Plugin_Template/libraries/MaichimpConnect.php
@@ -0,0 +1,29 @@
+<?php
+// call config and mailchimp files to grab the list
+$folderName = dirname(dirname(dirname(__FILE__)));
+require_once $folderName.'MCAPI.class.php';
+require_once $folderName.'/config.inc.php';
+class MailchimpConnect
+{
+  function apiConnect($apikey)
+	{
+      $api = new MCAPI($apikey);
+      $retval = $api->lists();
+	    $error = false;
+      if ($api->errorCode)	// if any error return by mailchimp
+      {
+          $mailChimpList = '<div class="mailchimpError"><div class="error-text">Unable to load lists!'.$api->errorMessage.'</div></div>';
+    			$error = true;
+    			unset($api);
+      }
+      else 	// prepare the mailchimp list and assign it to js's mailChimpList variable
+      {
+          $mailChimpList = '<option value="" webId="" class="bronze-info">No list selected..</option>';
+		      foreach ($retval['data'] as $list)
+          {
+              $mailChimpList.= '<option '.$selected.' value="'.$list['id'].'" webId = "'.$list['web_id'].'">'.$list['name'].'</option>';
+          }
+      }
+		  return array('list'=>$mailChimpList,'error'=>$error);
+	}
+}
